592d50b386285e0e34224fde59f37bd90503ac79
TAMURA Ikuho <130094749+tamuraikuho@users.noreply.github.com>
2023-08-05 01:31:03 +0900

    portfolio update 230805
    portfolio update 230805
    Add works

---
+++ script.js
@@ -0,0 +1,42 @@
+$(function(){
+  const cursor = $("#js-cursor");
+  const mouse = $("#js-mouse");
+  $(document).on("mousemove",function(e){
+      const x=e.clientX;
+      const y=e.clientY;
+      cursor.css({
+          "opacity": "1",
+          "transform": "translate(" + x + "px," + y + "px)",
+      });
+      setTimeout(function(){
+          mouse.css({
+              "opacity": "1",
+              "transform": "translate(" + x + "px," + y + "px)",
+          });
+      },150);
+  });
+});
+
+//ホバーグラデ
+
+$(function () {
+  var num = Math.floor(5 * Math.random());
+  $('.photo-caption').addClass('background' + num);
+});
+
+//サイト背景
+
+$(function () {
+  var num = Math.floor(5 * Math.random());
+  $('.body').addClass('background' + num);
+});
+
+//時計
+
+function showtime() {
+  var today = new Date();
+  $weekday = ['Su.', 'Mo.', 'Tu.', 'We.', 'Th.', 'Fr.', 'Sa.'];
+  month = today.getMonth() + 1;
+  $('#time').html(month + "." + today.getDate() + "." + $weekday[today.getDay()] + " " + today.getHours() + ":" + ('0' + today.getMinutes()).slice(-2) + ":" + ('0' + today.getSeconds()).slice(-2));
+}
+setInterval(showtime, 1000);
