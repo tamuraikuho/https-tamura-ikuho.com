e243ee19869072478110bcb38ddf99d239a65536
TAMURA Ikuho <130094749+tamuraikuho@users.noreply.github.com>
2023-08-31 13:29:44 +0900

    Update WEBsite
    Add Works, Add Loading, Update CSS

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ $(function(){
   });
 });
 
+//マウスグラデ
+
+$(function () {
+  var num = Math.floor(5 * Math.random());
+  $('#js-mouse').addClass('mousebg' + num);
+});
+
 //ホバーグラデ
 
 $(function () {
   var num = Math.floor(5 * Math.random());
   $('.photo-caption').addClass('background' + num);
-});
-
-//サイト背景
-
-$(function () {
-  var num = Math.floor(5 * Math.random());
-  $('.body').addClass('background' + num);
 });
 
 //時計
@@ -40,3 +40,9 @@ function showtime() {
   $('#time').html(month + "." + today.getDate() + "." + $weekday[today.getDay()] + " " + today.getHours() + ":" + ('0' + today.getMinutes()).slice(-2) + ":" + ('0' + today.getSeconds()).slice(-2));
 }
 setInterval(showtime, 1000);
+
+//ローディング
+
+$(function () {
+  $('#loading').addClass('loaded');
+});
